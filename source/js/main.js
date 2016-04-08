0fff26260cb40cf62ba9543a8d49a7027e99eca2
Keit Oliveira <itsme@keitoliveira.io>
2016-04-07 22:06:15 -0300

    script animation

--- source/js/main.js
+++ source/js/main.js
@@ -7,31 +7,71 @@ fis.functions = (function() {
   function init() {
     showContent();
     hideContent();
+    animationContent();
+  }
+
+  function add(nth, time) {
+    setTimeout(function() {
+        $('.area:nth-child('+nth+')').addClass('is-animation');
+      }, time);
+  }
+
+  function remove(nth, time) {
+    setTimeout(function() {
+        $('.area:nth-child('+nth+')').removeClass('is-animation');
+      }, time);
+  }
+
+  function animationContent(el) {
+
+
+    $('.area').on({
+      mouseover: function() {
+        var nth  = [1, 2, 3, 4, 5, 6];
+
+        nth.sort(function(a) {
+            return Math.random() - 0.5;
+        });
+
+        nth.filter(function(i) {
+          if($(this).index()+1 == i) {
+            return;
+          }
+
+          setTimeout(function() {
+            $('.area:nth-child('+i+')').addClass('is-animation');
+          }, 100 * i);
+        }.bind(this));
+      },
+      mouseout: function() {
+        $('.area').removeClass('is-animation');
+      }
+    });
   }
 
   function showContent() {
     $('.btn-area').on('click', function(e){
       e.preventDefault();
-      animationClass(this);
-      $('body').addClass('is-active')
-      $(this).closest('.area').addClass('is-show')
+      // animationClass(this);
+      $('body').addClass('is-active');
+      $(this).closest('.area').addClass('is-show');
     });
   }
 
   function hideContent() {
     $('.btn-close').on('click', function(){
-      $('body').removeClass('is-active')
-      animationClass(this);
+      $('body').removeClass('is-active');
+      // animationClass(this);
       $(this).closest('.area').removeClass('is-show');
     });
   }
 
-  function animationClass(el){
-    $(el).closest('.area').addClass('is-animation')
-    setTimeout(function(){
-      $(el).closest('.area').removeClass('is-animation');
-    },300)
-  }
+  // function animationClass(el){
+  //   $(el).closest('.area').addClass('is-animation');
+  //   setTimeout(function(){
+  //     $(el).closest('.area').removeClass('is-animation');
+  //   },300);
+  // }
 
   return {
     init: init

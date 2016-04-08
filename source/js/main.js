9019b28aecbcb6af5420af08bf79c2b7b884f218
Deivid Marques <deividmarques@gmail.com>
2016-04-07 23:20:08 -0300

    fix animation

--- source/js/main.js
+++ source/js/main.js
@@ -11,9 +11,9 @@ fis.functions = (function() {
   }
 
   function animation() {
-    $('.area').on({
+    $('.is-normal .area').on({
       mouseover: function() {
-        $('.area').removeClass('is-animation');
+        $('.is-normal .area').removeClass('is-animation');
 
         var nth  = [1, 2, 3, 4, 5, 6];
 
@@ -27,12 +27,12 @@ fis.functions = (function() {
           }
 
           setTimeout(function() {
-            $('.area:nth-child('+i+')').addClass('is-animation');
+            $('.is-normal .area:nth-child('+i+')').addClass('is-animation');
           }, 100 * i);
         }.bind(this));
       },
       mouseout: function() {
-        $('.area').removeClass('is-animation');
+        $('.is-normal .area').removeClass('is-animation');
       }
     });
   }
@@ -40,26 +40,20 @@ fis.functions = (function() {
   function showContent() {
     $('.btn-area').on('click', function(e){
       e.preventDefault();
-      // animationClass(this);
-      $('body').addClass('is-active');
+      $('body').removeClass('is-normal');
       $(this).closest('.area').addClass('is-show');
     });
   }
 
   function hideContent() {
     $('.btn-close').on('click', function(){
-      $('body').removeClass('is-active');
-      // animationClass(this);
+
+      setTimeout(function(){
+        $('body').addClass('is-normal');
+      },300);
       $(this).closest('.area').removeClass('is-show');
     });
   }
-
-  // function animationClass(el){
-  //   $(el).closest('.area').addClass('is-animation');
-  //   setTimeout(function(){
-  //     $(el).closest('.area').removeClass('is-animation');
-  //   },300);
-  // }
 
   return {
     init: init
